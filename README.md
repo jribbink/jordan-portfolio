b9200db70d502c85d522e339187741f4fc2b95d5
Jordan Ribbink <jribbink@telus.net>
2022-02-13 22:52:23 -0800

    Setup eslint with airbnb rules

---
+++ README.md
@@ -0,0 +1,5 @@
+# jordan-portfolio
+This is the source code for my portfolio hosted at ![my personal website](https://jordan.ribbink.ca).
+
+# Technologies
+React + Material UI
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material'
-import { Menu as MenuIcon } from '@mui/icons-material'
+import React, { ReactElement } from 'react';
+import {
+  AppBar, Button, Container, IconButton, Toolbar, Typography,
+} from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 
-class App extends React.Component {
- render() {
-  return(
+function App(): ReactElement {
+  return (
     <div>
-        <AppBar position="fixed">
-            <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                News
-            </Typography>
-            <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
-        <Container>
-            Hello
-        </Container>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        Hello
+      </Container>
     </div>
   );
- }
 }
-export default App
+export default App;
