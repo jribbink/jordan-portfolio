a77d45e0260d1e1d63e51b39118ab8ca0f0576fe
Jordan Ribbink <jribbink@telus.net>
2022-02-13 20:32:13 -0800

    Setup initial react app

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material'
+import { Menu as MenuIcon } from '@mui/icons-material'
+
+class App extends React.Component {
+ render() {
+  return(
+    <div>
+        <AppBar position="fixed">
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+            </Typography>
+            <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+        <Container>
+            Hello
+        </Container>
+    </div>
+  );
+ }
+}
+export default App
